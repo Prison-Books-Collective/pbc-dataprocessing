74c85a2e5ef75d7ce29cd862b10c79d275bb9ce2
samah <samahmajadla@gmail.com>
2020-12-04 14:26:35 -0500

    unicode and dashes

--- getAllUnicodeTitles.py
+++ getAllUnicodeTitles.py
@@ -8,12 +8,42 @@
 
 #in mysql - replace 978978 w "" if isbn10 is like 978978% and is more than 10 long
 #
+
 import requests
 import json
+import re
 
-url = "http://localhost:8080/getBooksTitleContaining?targetString=u0"
-response = requests.get(url)
-if response.status_code == 200:
-	responsejson = response.json()
-	with open('unicodeTitles.txt', 'w') as outfile:
-		json.dump(responsejson, outfile)
+#url = "http://localhost:8080/getBooksTitleContaining?targetString=u0"
+#response = requests.get(url)
+#if response.status_code == 200:
+#	responsejson = response.json()
+#	with open('unicodeTitles.txt', 'w') as outfile:
+#		json.dump(responsejson, outfile)
+file = open('unicodeTitles.txt')
+data = json.loads(file.read())
+file.close()
+
+i = 1
+length = len(data)
+errors = []
+url = "http://localhost:8080/updateBook"
+for book in data:
+	print(f'{i} out of {length}, ISBN10 {book["isbn10"]} ISBN13 {book["isbn13"]}')
+	pattern = re.compile('''u0\w{3}''')
+	bookTitle = book["title"]
+	convertedMatches = []
+	for match in pattern.finditer(bookTitle):
+		matchStart = match.start()
+		try:
+			convertedMatches.append((match.group(), chr(int(bookTitle[matchStart+1:matchStart+5], 16))))
+		except ValueError:
+			errors.append((book["isbn10"], book["isbn13"]))
+	if (len(convertedMatches) > 0):
+		for match in convertedMatches:
+			book["title"] = book["title"].replace(match[0], match[1])
+		requests.put(url, json = book)
+	i = i+ 1
+print(errors)
+errorsFile = open('unicodeERRORS.txt', "w")
+errorsFile.write(errors)
+
--- removeDashesAndSpacesFromIsbns.py
+++ removeDashesAndSpacesFromIsbns.py
@@ -4,6 +4,8 @@
 	# search for new isbn
 	# if it exists:
 		# delete book with '-'
+	#else
+		# update book
 
 #get all books with ' ' in the isbn10
 #for each book
@@ -25,3 +27,24 @@
 	# search for new isbn
 	# if it exists:
 		# delete book with ' '
+import json
+import requests
+
+file = open('dashes.txt')
+data = json.loads(file.read())
+file.close()
+
+i = 1
+length = len(data)
+errors = []
+getUrl = "http://localhost:8080/getIsbn13?isbn13="
+deleteUrl = "http://localhost:8080/deleteBook?id="
+updateUrl = "http://localhost:8080/updateIsbn13?id="
+
+for book in data:
+	book["isbn13"] = book["isbn13"].replace('-','')
+	response = requests.put(updateUrl+str(book["id"])+"&isbn13="+book["isbn13"])
+	
+	print(f'{i} out of {length}, ISBN10 {book["isbn10"]} ISBN13 {book["isbn13"]}')
+	i = i+ 1
+
